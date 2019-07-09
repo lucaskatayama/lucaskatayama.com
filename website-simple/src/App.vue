f70c27293289c179039a57e6d940479d36e4f87b
Lucas Katayama <lucaskatayama@gmail.com>
2019-07-09 15:29:31 -0300

    simple version

---
+++ website-simple/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <div class="container-wrapper">
+      <div class="container">
+        <Home />
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+import Home from "@/views/Home.vue";
+export default {
+  components: {
+    Home
+  }
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  background-color: #fff;
+}
+
+body {
+  height: 100vh;
+}
+
+.app {
+  font-family: "Zilla Slab", serif;
+  font-size: 2em;
+  color: #2c3e50e1;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  .container-wrapper {
+    padding: 0 10vw;
+    flex-direction: column;
+  }
+}
+</style>
---
+++ website-simple/src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="home">
+    <h1>Hello World!</h1>
+    <h2>My name is Lucas Katayama</h2>
+    <section>
+      <p>
+        and I am Brazilian Software Developer, building web and mobile
+        applications, <br />
+        with 5 years of experience willing to move abroad to improve my skills
+        in development.
+      </p>
+
+      <p>I am self-taught and I ❤ new technologies and photography.</p>
+    </section>
+    <section class="links">
+      <div class="links">
+        <template v-for="link in links">
+          <a target="_blank" :href="link.link" :key="link.label">{{
+            link.label
+          }}</a>&nbsp;
+        </template>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          link:
+            "https://files.lucaskatayama.com/cv.pdf?utm_source=homepage&utm_campaign=resume&utm_medium=link&utm_term=resume&utm_content=resume",
+          label: "résumé"
+        },
+        {
+          link: "https://www.linkedin.com/in/lucaskatayama",
+          label: "linkedin"
+        },
+        {
+          link: "https://github.com/lucaskatayama",
+          label: "github"
+        },
+        {
+          link: "https://twitter.com/@lucaskatayama",
+          label: "twitter"
+        },
+        {
+          link: "https://medium.lucaskatayama.com",
+          label: "medium"
+        },
+        {
+          link: "https://stackoverflow.com/users/1181036/lucas-katayama",
+          label: "stack overflow"
+        },
+        {
+          link: "https://instagram.com/bylucask",
+          label: "instagram"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$color-link: #3175bf;
+section {
+  margin-bottom: 2em;
+  &.links {
+    font-size: 0.8em;
+    a {
+      color: $color-link;
+      text-transform: lowercase;
+    }
+  }
+}
+</style>
